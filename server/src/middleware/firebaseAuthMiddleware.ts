48f3f184796349f57a4e60cfc201590bc6026879
bifi <dennis.kaminski95@gmail.com>
2021-09-13 20:44:12 +0200

    Unified typescript version

--- server/src/middleware/firebaseAuthMiddleware.ts
+++ server/src/middleware/firebaseAuthMiddleware.ts
@@ -1,7 +1,6 @@
+import io from "socket.io";
 import { ExtendedError } from "socket.io/dist/namespace";
 import { DefaultEventsMap } from "socket.io/dist/typed-events";
-import io, { Server, ServerOptions } from "socket.io";
-import * as admin from "firebase-admin";
 import { useAuthToken } from "../hooks/socketHooks";
 
 export async function firebaseAuthMiddleware( socket: io.Socket<DefaultEventsMap, DefaultEventsMap>, next: ( error?: ExtendedError | undefined ) => void )
@@ -11,7 +10,7 @@ export async function firebaseAuthMiddleware( socket: io.Socket<DefaultEventsMap
         await useAuthToken( socket );
         next();
     }
-    catch ( error )
+    catch ( error: any )
     {
         console.error( error );
         next( new Error( error.message ) );
